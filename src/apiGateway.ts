545f5af1efe143b47e16babc4b567fcf24f3da5e
Teodoro Bertolozzi Mendes <teobmendes@gmail.com>
2021-01-17 20:29:48 -0300

    long time no saving

--- src/apiGateway.ts
+++ src/apiGateway.ts
@@ -1,29 +1,15 @@
 import * as awsx from "@pulumi/awsx";
 import {lambda} from "./pulumiLambdas";
-import * as aws from "@pulumi/aws"
-import {bucket} from "./buckets";
-
-const apis3proxy = (bucket: aws.s3.Bucket): awsx.apigateway.IntegrationRoute => {
-    return {
-        path: "/post_to_s3",
-        target: {
-            type: "aws_proxy",
-            uri: `arn:aws:apigateway:us-east-1:s3:action/PutObject&Bucket={${bucket.bucket}}&Key={${Date.now()}}`
-        }
-    }
-}
 
 // create API
 let apiGateway = new awsx.apigateway.API(`payloads-api-meetup-api-gateway`, {
     routes: [
-        // {
-        //     path: "/post_to_s3",
-        //     method: "POST",
-        //     eventHandler: lambda
-        // }
-        apis3proxy(bucket)
+        {
+            path: "/post_to_s3",
+            method: "POST",
+            eventHandler: lambda
+        }
     ]
 })
 
-
-export const apiUrl = apiGateway.url
+export {apiGateway}
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,155 @@
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import {STACK} from "./pulumiStack";
+import {Request, Response} from "@pulumi/awsx/apigateway/api";
+import {lambda} from "./pulumiLambdas";
 
 
 interface NeverMissAWebhookInterface {
-    deliveryEndpoint: string
-
-    api: aws.apigatewayv2.Api
-    apiIntegration: aws.apigateway.Integration
-
-    postToSQS(message: string): string
-
+    withDeliveryEndpoint(url: string): NeverMissAWebhook
+    withGlobalPrefix(globalPrefix: string): NeverMissAWebhook
+    withOptionalSQSConfiguration(args: aws.sqs.QueueArgs): NeverMissAWebhook
+    withDirectSqsIntegration(path: string): NeverMissAWebhook
+    withPayloadContentSaverIntermediate(): NeverMissAWebhook
 }
 
 // BucketNotification
-export class NeverMissAWebhook {
+export class NeverMissAWebhook implements NeverMissAWebhookInterface {
+
+    /**
+     * The endpoint to post to
+     * */
+    private deliveryEndpoint: string | null = null
+
+    /**
+     * The queue where requests will be posted to.
+     * */
+    private sqsDeliveryQueue: aws.sqs.Queue | null = null
+
+    /**
+     * The queue args that might be overwritten by the user
+     * */
+    private queueArgs: aws.sqs.QueueArgs = {
+        // TODO define best attributes
+    }
+
+    /**
+     * The prefix that's going to be attached to every single service
+     * */
+    private globalPrefix: string | null = null
+
+    /**
+     * The flag that indicates if the chosen method was direct integration
+     * or the s3 payload saving method
+     * */
+    private directDelivery: boolean = true
 
     /**
      * If the chosen method is to directly post the message to SQS,
      * then the Api Gateway acts as a simple proxy, and we don't need
      * a lambda to manage the payload before the posting action.
      * */
-    private sqsProxyApi: aws.apigatewayv2.Api | null = null
-    private sqsProxyApiIntegration: aws.apigatewayv2.Integration | null = null
+    private sqsProxyApi: awsx.apigateway.API | null = null
+
+    /**
+     * The lambda function responsible for taking the request body incoming from api gateway
+     * and posting it to the queue
+     * */
+    private sqsProxyApiLambdaPayloadRedirector: aws.lambda.EventHandler<Request, Response> | null = null
 
     /**
      * If the chosen method is to directly save the message to an S3
-     * object or to check the payload size before deciding
+     * object and then share it's key to an SQS queue, then we need to provide
+     * a simple Lambda function that fulfills our proxy needs.
      * */
-    private genericApi: awsx.apigateway.API | null = null
+    private s3ProxyApi: awsx.apigateway.API | null = null
 
-    private lambdaSavePayloadInS3AndSQSPost
+    /**
+     * The functions that takes the request body incoming from api gateway
+     * and saves it as a json file in an S3 bucket
+     * */
+    private s3ProxyApiLambdaPayloadSaver: aws.lambda.EventHandler<Request, Response> | null = null
+
+    /**
+     * The bucket used for saving payloads
+     * */
+    private s3ProxyApiBucket: aws.s3.Bucket | null = null
+
+    /**
+     * The lambda that handles SQS messages for a simple delivery attempt.
+     * */
+    private sqsEventHandlerSimpleDeliveryAttempt: aws.sqs.QueueEventHandler | null = null
+
+    /**
+     * The lambda that handles SQS messages for delivery attempts from
+     * saved payloads.
+     * */
+    private sqsEventHandlerDeliveryFromSavedPayload: aws.sqs.QueueEventHandler | null = null
+
+    private constructor() {}
+
+    public static builder() {
+        return new NeverMissAWebhook()
+    }
+
+    public withDeliveryEndpoint(url: string) {
+        this.deliveryEndpoint = url
+        return this
+    }
+
+    public withGlobalPrefix(globalPrefix: string) {
+        this.globalPrefix = globalPrefix
+        return this
+    }
+
+    public withOptionalSQSConfiguration(args: aws.sqs.QueueArgs) {
+        this.queueArgs = args
+        return this;
+    }
+
+    public withDirectSqsIntegration(path: string) {
+        this.directDelivery = true
+
+        this.sqsDeliveryQueue = new aws.sqs.Queue(`${this.globalPrefix}-queue-${STACK}`, this.queueArgs)
+
+
+
+        this.sqsProxyApi = new awsx.apigateway.API(`${this.globalPrefix}-sqs-proxy-api-${STACK}`, {
+            routes: [
+                {
+                    path: "/",
+                    method: "POST",
+                    eventHandler: lambda
+                }
+            ]
+        })
+
+        return this
+    }
+
+    public withPayloadContentSaverIntermediate() {
+        this.directDelivery = false
+        return this
+    }
+
+    private builQueue() {
+
+    }
+
+    private createBucketNotificationToSQS() {
+        new aws.s3.BucketNotification(`${this.globalPrefix}-bucket-notification-${STACK}`, {
+            bucket: this.s3ProxyApiBucket!.bucket,
+            queues: [
+                {
+                    events: ["s3:ObjectCreated:*"],
+                    queueArn: this.sqsDeliveryQueue!.arn
+                }
+            ]
+        })
+    }
+
+
+
 
 }
+
