5e23230500cc0c628ce34d239ffb7a57d285673c
Teodoro Bertolozzi Mendes <teobmendes@gmail.com>
2021-01-17 00:02:32 -0300

    posting payload to S3

--- Readme.md
+++ Readme.md
@@ -5,3 +5,7 @@ $ pulumi config set aws:region <region>
 
 
 https://medium.com/lego-engineering/functionless-s3-integration-inside-a-serverless-casserole-part-1-b300085eea78
+https://chipperci.com/news/never-miss-a-webhook
+https://medium.com/@adamboazbecker/guide-to-connecting-aws-lambda-to-s3-with-pulumi-15393df8bac7
+
+Error creating S3 bucket: AuthorizationHeaderMalformed: The authorization header is malformed; the region 'us-east-1' is wrong; expecting 'us-west-2'
--- index.ts
+++ index.ts
@@ -1,17 +1,103 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+const pulumi = require("@pulumi/pulumi");
+const aws = require("@pulumi/aws");
+const awsx = require("@pulumi/awsx");
 
-const api = new awsx.apigateway.API("hello-world", {
-    routes: [{
-        path: "/",
-        method: "POST",
+const STACK = pulumi.getStack();
 
-        eventHandler: async (event) => {
-            return {
-                statusCode: 200,
-                body: "Hello, world!",
-            };
-        },
-    }],
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("payloads-bucket", {
+    bucket: `nmaw-${STACK}`
+});
+
+
+const lambdaRole = new aws.iam.Role(`role-payloads-api`, {
+    assumeRolePolicy: `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+`,
 })
+
+// Policy for allowing Lambda to interact with S3
+const lambdaS3Policy = new aws.iam.Policy(`post-to-s3-policy`, {
+    description: "IAM policy for Lambda to interact with S3",
+    path: "/",
+    policy: bucket.arn.apply((bucketArn: any) => `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:PutObject",
+      "Resource": "${bucketArn}/*",
+      "Effect": "Allow"
+    }
+  ]}`)
+})
+
+// Attach the policies to the Lambda role
+new aws.iam.RolePolicyAttachment(`post-to-s3-policy-attachment`, {
+    policyArn: lambdaS3Policy.arn,
+    role: lambdaRole.name
+})
+
+
+const lambdaFunction = async (event: any) => {
+    const AWS = require('aws-sdk')
+    const s3 = new AWS.S3()
+    // decode the body of the event
+    const payloadBuffer = new Buffer(event.body, 'base64')
+    const payload = payloadBuffer.toString('ascii')
+    const putParams = {
+        Bucket: process.env.S3_BUCKET, // We'll read the .env variable
+        Key: `${new Date().getTime()}.json`, // We'll use the timestamp
+        Body: payload
+    }
+
+    await new Promise((resolve, reject) => {
+        s3.putObject(putParams, function (err: any, data: any) {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+    return {
+        statusCode: 200,
+        body: "Success"
+    }
+}
+const lambda = new aws.lambda.CallbackFunction(`payloads-api-meetup-lambda`, {
+    name: `payloads-api-meetup-lambda-${STACK}`,
+    runtime: "nodejs12.x",
+    role: lambdaRole,
+    callback: lambdaFunction,
+    environment: {
+        variables: {
+            S3_BUCKET: bucket.id
+        }
+    },
+})
+
+// create API
+let apiGateway = new awsx.apigateway.API(`payloads-api-meetup-api-gateway`, {
+    routes: [
+        {
+            path: "/post_to_s3",
+            method: "POST",
+            eventHandler: lambda
+        }
+    ]
+})
+
+
+// Export the name of the bucket
+exports.bucketName = bucket.id
+exports.apiGateway = apiGateway.url
